72aaec361e5ca3143c635fa5b67ff114ab8e80b4
Mariak Achuoth <achuothmariak@gmail.com>
2024-05-13 15:21:30 -0400

    Navbar section
    Done!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { About, Footer, Header, Projects, Work } from './Container';
 import { Navbar } from './Components';
+import './App.scss';
 const App = () => {
   return (
     <div className="app">
